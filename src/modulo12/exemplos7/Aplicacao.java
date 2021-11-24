0c359815f5260fd77dca1890139e17da7bbca228
ezbueno <ezandrobueno@yahoo.com.br>
2021-11-24 18:41:39 -0300

    Mapas

--- src/modulo12/exemplos7/Aplicacao.java
+++ src/modulo12/exemplos7/Aplicacao.java
@@ -1,9 +1,40 @@
 package modulo12.exemplos7;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Aplicacao {
 
 	public static void main(String[] args) {
 		
+		Map<Integer, Produto> map = new HashMap<>();
 		
+		Produto p1 = new Produto(1, "Produto 1");
+		Produto p2 = new Produto(2, "Produto 2");
+		Produto p3 = new Produto(3, "Produto 3");
+		
+		map.put(1, p1);
+		map.put(2, p2);
+		map.put(3, p3);
+		
+		Produto p = map.get(2);
+		System.out.println(p);
+		
+		Set<Integer> keys = map.keySet();
+		for (Integer key : keys) {
+			System.out.println(key);
+		}
+		
+		Collection<Produto> values = map.values();
+		for (Produto prod : values) {
+			System.out.println(prod);
+		}
+		
+		Set<Map.Entry<Integer, Produto>> entries = map.entrySet();
+		for (Map.Entry<Integer, Produto> entry : entries) {
+			System.out.println(entry.getKey() + " => " + entry.getValue());
+		}
 	}
 }
---
+++ src/modulo12/exemplos7/Produto.java
@@ -0,0 +1,25 @@
+package modulo12.exemplos7;
+
+public class Produto {
+
+	private int id;
+	private String descricao;
+
+	public Produto(int id, String descricao) {
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	@Override
+	public String toString() {
+		return id + ", " + descricao;
+	}
+}
