a923cb255540b393eab638b1646a7d90c07277dc
ezbueno <ezandrobueno@yahoo.com.br>
2021-12-01 19:38:04 -0300

    Módulo 13 - Finalizado

--- src/modulo13/exercicios2/Aplicacao.java
+++ src/modulo13/exercicios2/Aplicacao.java
@@ -10,6 +10,31 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+/**
+ * @author Ezandro Bueno
+ * Crie um aplicativo capaz de gravar os dados de objetos em um arquivo em forma de bytes e 
+ * também de lê-los.
+ * Primeiramente crie duas classes, Livro e Autor, de acordo com o demonstrado no diagrama 
+ * abaixo:
+ * Livro			Autor
+ * titulo: String	nome: String
+ * numPaginas: int	dataNascimento: Date
+ * autor: Autor
+ * Crie também uma interface Recordable, que declara dois métodos:
+ * public void read(DataInputStream in) throws IOException 
+ * public void write(DataOutputStream out) throws IOException
+ * Faça com que Livro e Autor implementem esta interface. Classes que implementam esta 
+ * interface devem codificar a forma como seus próprios atributos são gravados e lidos no 
+ * arquivo, através de chamadas aos objetos in e out.
+ * A aplicação deve pedir para que o usuário escolha entre duas opções: gravar ou ler os dados. 
+ * Caso a opção de gravar seja escolhida, dois objetos Livro devem ser criados e gravados em 
+ * um arquivo. Já se a opção de leitura for escolhida, estes dois objetos devem ser lidos do 
+ * arquivo e impressos no console. 
+ * Dica: Objetos DataInputStream e DataOutputStream possuem métodos para ler/gravar 
+ * diversos tipos de dados, como String, int, double, etc. Eles encapsulam uma InputStream e 
+ * uma OutputStream, respectivamente. Tenha em mente que a ordem de leitura dos dados do 
+ * arquivo deve ser a mesma ordem de escrita.
+ */
 public class Aplicacao {
 
 	private static final String FILE = "livros.txt";
